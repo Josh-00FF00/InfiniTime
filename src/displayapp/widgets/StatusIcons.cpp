e6904eedddd5fc70b00819908b9f4e9f58a84415
Joshua <4805803+Josh-00FF00@users.noreply.github.com>
2023-05-09 21:14:09 +0100

    Adds percentages to Digital Watch Face

--- src/displayapp/widgets/StatusIcons.cpp
+++ src/displayapp/widgets/StatusIcons.cpp
@@ -11,11 +11,14 @@ void StatusIcons::Create() {
   container = lv_cont_create(lv_scr_act(), nullptr);
   lv_cont_set_layout(container, LV_LAYOUT_ROW_TOP);
   lv_cont_set_fit(container, LV_FIT_TIGHT);
-  lv_obj_set_style_local_pad_inner(container, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 5);
+  lv_obj_set_style_local_pad_inner(container, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 6);
   lv_obj_set_style_local_bg_opa(container, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_OPA_TRANSP);
 
   bleIcon = lv_label_create(container, nullptr);
   lv_label_set_text_static(bleIcon, Screens::Symbols::bluetooth);
+
+  batteryPercentage = lv_label_create(container, nullptr);
+  lv_label_set_text_static(batteryPercentage, "  %");
 
   batteryPlug = lv_label_create(container, nullptr);
   lv_label_set_text_static(batteryPlug, Screens::Symbols::plug);
@@ -34,6 +37,8 @@ void StatusIcons::Update() {
   batteryPercentRemaining = batteryController.PercentRemaining();
   if (batteryPercentRemaining.IsUpdated()) {
     auto batteryPercent = batteryPercentRemaining.Get();
+
+    lv_label_set_text_fmt(batteryPercentage, "%d%%", batteryPercent);
     batteryIcon.SetBatteryPercentage(batteryPercent);
   }
 
